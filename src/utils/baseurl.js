c9d3088a00e121caaa286f8d5bb21a19e99b79e4
AJAY0993 <ajaymishra5277919886@gmail.com>
2024-02-12 17:03:53 +0530

    set up production url

--- src/utils/baseurl.js
+++ src/utils/baseurl.js
@@ -1,1 +1,1 @@
-export const BASE_URL = 'https://video-library-api-2-0.onrender.com/api/v1/'
+export const BASE_URL = import.meta.env.PROD ? "https://video-library-api-2-0.onrender.com/api/v1/" : "http://localhost:3000/api/v1/" 
